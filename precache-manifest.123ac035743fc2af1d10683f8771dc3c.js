dc5c6e1c7c04931399d2713530b3d40d8bad58fe
Alvis Chew <chewjta@gmail.com>
2020-05-18 19:20:23 +0800

    Updates

---
+++ precache-manifest.123ac035743fc2af1d10683f8771dc3c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ed20524d455715566517c08eb4866b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0d1f9abc72a2ff01d11",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "57d637b0b1b50a2fa645",
+    "url": "/static/css/main.f5ee2ef6.chunk.css"
+  },
+  {
+    "revision": "b0d1f9abc72a2ff01d11",
+    "url": "/static/js/2.b4bf1e85.chunk.js"
+  },
+  {
+    "revision": "a1da56f7bda213f89d46a985ed38ecb4",
+    "url": "/static/js/2.b4bf1e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d637b0b1b50a2fa645",
+    "url": "/static/js/main.96b3d107.chunk.js"
+  },
+  {
+    "revision": "24ab20a649fb4bb07b98",
+    "url": "/static/js/runtime-main.7fac44c2.js"
+  },
+  {
+    "revision": "1b2c6224d5805de3d3b96f4c482a2ce9",
+    "url": "/static/media/calculator.1b2c6224.PNG"
+  },
+  {
+    "revision": "8cf3e63a427099ab738999e198bf19a1",
+    "url": "/static/media/choropleth.8cf3e63a.PNG"
+  },
+  {
+    "revision": "7bcc004347ba83ffec14850a9f8c37c4",
+    "url": "/static/media/drummachine.7bcc0043.PNG"
+  },
+  {
+    "revision": "ca9143f1f32b044634eff6b391c8f0d2",
+    "url": "/static/media/exercisetracker.ca9143f1.PNG"
+  },
+  {
+    "revision": "2723b3929f5704c7ef31e54024a7d9d5",
+    "url": "/static/media/heatmap.2723b392.PNG"
+  },
+  {
+    "revision": "332145a5cae3f9ba034812f86ea53ecc",
+    "url": "/static/media/metadata.332145a5.PNG"
+  },
+  {
+    "revision": "61e9b03ec73b37637e8bddeaca033a8c",
+    "url": "/static/media/picfinder.61e9b03e.PNG"
+  },
+  {
+    "revision": "a4114cb25f61696fe7957685d56b0c76",
+    "url": "/static/media/pomodoro.a4114cb2.PNG"
+  },
+  {
+    "revision": "cbf38932d5c347f94b41d7ba6677c87c",
+    "url": "/static/media/treemap.cbf38932.PNG"
+  },
+  {
+    "revision": "5cb464362091374271282a0d15928988",
+    "url": "/static/media/twitchclone.5cb46436.PNG"
+  },
+  {
+    "revision": "147feacf433b01936417322394ba3dc3",
+    "url": "/static/media/urlshort.147feacf.PNG"
+  },
+  {
+    "revision": "da431ca85a91a601e12fc123c66dfcab",
+    "url": "/static/media/youtubeclone.da431ca8.PNG"
+  }
+]);
